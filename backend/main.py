56f1440e7f8e0c4a63aaed1d2f934ba494923b4a
dixitt5 <dixit@incubyte.co>
2025-04-18 16:17:16 +0530

    setup: update main file

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ load_dotenv(override=True)
 
 if __name__ == "__main__":
     host = os.getenv("API_HOST", "0.0.0.0")
-    port = int(os.getenv("API_PORT", "8000"))
+    port = int(os.getenv("PORT", "8000"))
     debug = os.getenv("API_DEBUG", "false").lower() == "true"
 
     uvicorn.run("app.main:app", host=host, port=port, reload=debug)
